0df578488d7b54965354d7c2e29d9200b496e2ca
Shivam-Deswal23 <137677346+Shivam-Deswal23@users.noreply.github.com>
2024-04-10 16:32:56 +0530

    rotate-array.java

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,37 +1,5 @@
 class Solution {
     public void rotate(int[] arr, int k) {
-        // int n = nums.length;
-
-        // for(int j=0; j<k; j++){
-        // int temp = nums[n-1];
-
-        // for(int i=n-1; i>0; i--){
-        //     nums[i] = nums[i-1];
-        // }
-        // nums[0] = temp;
-        // }
-
-        // for(int i=0; i<n; i++){
-        //     System.out.print(nums[i]+" ");
-        // }
-        // int n = arr.length;
-        // k = k % n; // to handle cases where k is larger than n
-        // if (k < 0) {
-        //     k = n + k; // to handle negative values of k
-        // }
-
-        // for (int i = 0; i < k; i++) {
-        //     int temp = arr[n - 1];
-        //     for (int j = n - 1; j > 0; j--) {
-        //         arr[j] = arr[j - 1];
-        //     }
-        //     arr[0] = temp;
-        // }
-
-        // for (int i = 0; i < n; i++) {
-        //     System.out.print(arr[i] + " ");
-        // }
-
         int n = arr.length;
         k = k % n; // to handle cases where k is larger than n
         if (k < 0) {
